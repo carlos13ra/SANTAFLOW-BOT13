06a0a6da9d1ea8d2c3184a903c72c4f3b3bc2f56
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-11-27 17:49:26 -0500

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,20 +1,22 @@
 import speed from 'performance-now'
-import { spawn, exec, execSync } from 'child_process'
+import { exec } from 'child_process'
 
 let handler = async (m, { conn }) => {
-         let timestamp = speed();
-         let latensi = speed() - timestamp;
-         exec(`neofetch --stdout`, (error, stdout, stderr) => {
-          let child = stdout.toString("utf-8");
-          let ssd = child.replace(/Memory:/, "Ram:");
+    let timestamp = speed();
+    let latensi = speed() - timestamp;
 
-          conn.reply(m.chat, `┏━❖『 ⚡ 𝐄𝐒𝐓𝐀𝐃𝐎 𝐎𝐍𝐋𝐈𝐍𝐄 』❖━┓
+    exec(`neofetch --stdout`, (error, stdout, stderr) => {
+        let child = stdout.toString("utf-8");
+        let ssd = child.replace(/Memory:/, "Ram:");
+
+        conn.reply(m.chat, `┏━❖『 ⚡ 𝐄𝐒𝐓𝐀𝐃𝐎 𝐎𝐍𝐋𝐈𝐍𝐄 』❖━┓
 ┃ 🖤 *Sistema activo y estable.*
 ┃ ⚡ 𝐓𝐢𝐞𝐦𝐩𝐨: ${latensi.toFixed(4)}ms
 ┃ ❝  *! Pong ¡*❞
-┗━━━━━━━━━━━━━━━━━━━┛`, m, rcanal);
-            });
-}
+┗━━━━━━━━━━━━━━━━━━━┛`, m);
+    });
+};
+
 handler.help = ['ping']
 handler.tags = ['info']
 handler.command = ['ping', 'p']
