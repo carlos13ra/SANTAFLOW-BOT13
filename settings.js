b65c0031c94ea0d637741744e3006cd617db2b56
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-11-27 17:11:52 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,8 +15,8 @@ global.botNumber = ''
 
 global.owner = [
 // <-- Número @s.whatsapp.net -->
-  ['51984169553', '🜲 Propietario 🜲', true],
-  ['51984169553', 'Carlos.rv', true],
+  ['51900922660', '🜲 Propietario 🜲', true],
+  ['51900922660', 'Carlos.rv', true],
   ['51946200884', ':v', true],
   ['51919199620', ':v', true ],
   ['51965763942', 'ShadowCore 🌿', true],
@@ -33,38 +33,38 @@ global.owner = [
   
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
-global.mods = ['51984169553']
-global.suittag = ['51984169553'] 
-global.prems = ['51984169553']
+global.mods = ['51900922660']
+global.suittag = ['51900922660'] 
+global.prems = ['51900922660']
 
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
 global.libreria = 'Baileys'
 global.baileys = 'V 6.7.17' 
 global.vs = '2.2.5'
-global.nameqr = '✦⃟⚡ GOJO•Bot⚡⃟✦'
-global.namebot = '⸸ 𝐆𝐎𝐉𝐎•𝐁𝐎𝐓 ⸸'
+global.nameqr = '✦⃟🥭SANTAFLOW•Bot✦'
+global.namebot = '⸸ 𝐒𝐀𝐍𝐓𝐀𝐅𝐋𝐎𝐖•𝐁𝐎𝐓 ⸸'
 global.sessions = 'Sessions'
 global.jadi = 'JadiBots' 
 global.shadowJadibts = true
 
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
-global.packname = '🏆 ⌬ 𝐆𝐨𝐣𝐨 𝑩𝒐𝒕 ⌬ 💥'
-global.botname = '☘️ 𝐆𝐎𝐉𝐎 𝐁𝐎𝐓'
-global.wm = '◈𝐆𝐎𝐉𝐎 𝐁𝐎𝐓◈'
-global.author = '⩇⃟🔋 𝑴𝒂𝒅𝒆 𝒃𝒚 𝐜𝐚𝐫𝐥𝐨𝐬.𝐫𝐯 ⩇⃟⚡'
+global.packname = '🎧 ⌬ 𝑺𝒂𝒏𝒕𝒂𝒇𝒍𝒐𝒘 𝑩𝒐𝒕 ⌬ 💥'
+global.botname = '💫 𝐒𝐀𝐍𝐓𝐀𝐅𝐋𝐎𝐖 𝐁𝐎𝐓'
+global.wm = '◈𝐒𝐀𝐍𝐓𝐀𝐅𝐋𝐎𝐖 𝐁𝐎𝐓◈'
+global.author = '⩇⃟💥 𝑴𝒂𝒅𝒆 𝒃𝒚 𝐜𝐚𝐫𝐥𝐨𝐬.𝐫𝐯 ⩇⃟📀'
 global.dev = '☘️ ミ💨 》𝑪𝑨𝑹𝑳𝑶𝑺.𝑹𝑽《 💥ミ 🌀'
 global.bot = '𝑺𝒂𝒏𝒕𝒂𝒇𝒍𝒐𝒘 𝒃𝒐𝒕'
-global.club = '🌱 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖻𝗒 carlos•Core 𝖢𝗅𝗎𝖻 ⚡'
-global.textbot = 'ɢᴏᴊᴏ ʙᴏᴛ✦ 𝕊ℍ𝔸𝔻𝕆𝕎•ℂ𝕆ℝ𝔼'
-global.etiqueta = '@sʜᴀᴅᴏᴡ°ᴄᴏʀᴇ'
+global.club = '❄️ 𝖯𝗈𝗐𝖾𝗋𝖾𝖽 𝖻𝗒 carlos•Core 𝖢𝗅𝗎𝖻 🥭'
+global.textbot = 'ɢᴏᴊᴏ ʙᴏᴛ✦ 𝑺𝒂𝒏𝒕𝒂𝒇𝒍𝒐𝒘'
+global.etiqueta = '@𝙲𝚊𝚛𝚕𝚘𝚜°ᴄᴏʀᴇ'
 
 //✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
 
 global.moneda = 'ᴇᴜʀᴏs💶'
 global.welcom1 = '💤 Bienvenido/a al grupo⚡\n❍ Edita con el comando *setwelcome*'
-global.welcom2 = '🔥 un miembro ha salido del partido🔥\n❍ Edita con el comando *setbye*'
+global.welcom2 = '🔥 un miembro ha salido del grupo🔥\n❍ Edita con el comando *setbye*'
 global.banner = 'https://i.postimg.cc/fbgJW3ND/93f25ee53428103d24f795274ad12ab3.jpg'
 global.avatar = 'https://i.postimg.cc/xjMhMVbG/personaje-satoru-gojo-jujutsu-kaisen-7799.jpg'
 global.logo = 'https://i.postimg.cc/Gpry5rSz/0x1900-000000-80-0-0.jpg'
@@ -83,9 +83,9 @@ global.correo = 'carlosramirezvillanueva30@gmail.com'
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363401008003732@newsletter',
-ch2: "120363401008003732@newsletter",
-ch3: "120363401008003732@newsletter"
+ch1: "120363404087331895@newsletter",
+ch2: "120363404087331895@newsletter",
+ch3: "120363404087331895@newsletter"
 }
 global.multiplier = 60
 
