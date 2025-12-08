ef85c3176c57d4cb9e2d19f803c5c2260446f0c2
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-12-08 10:11:18 -0500

    Update _audios.js

--- plugins/_audios.js
+++ plugins/_audios.js
@@ -65,7 +65,7 @@ this.sendPresenceUpdate('recording', m.chat)
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: fkontak })}
 
 if (chat.audios && m.text.match(/(Elmo sabe donde vives|Elmo sabe dónde vives|elmo|vives|de donde eres|eres de|sabes)/gi)) {    
-let vn = 'https://qu.ax/YsLt.mp3'
+let vn = 'https://jumpshare.com/s/nrU5RiehouygbkJ3SOoz'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: fkontak })}
 
