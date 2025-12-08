42990d6b066b7c3f3b56e9c907f4afbf163a2146
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-12-07 23:00:15 -0500

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
 try {
-let res = await fetch('https://github.com/carlos13ra/SANTAFLOW-BOT13.git')
+let res = await fetch('https://api.github.com/carlos13ra/SANTAFLOW-BOT13.git')
 
 if (!res.ok) throw new Error('Error al obtener datos del repositorio')
 let json = await res.json()
