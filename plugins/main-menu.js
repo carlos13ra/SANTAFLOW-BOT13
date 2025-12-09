3b4d1a3a3b831a73219a24018e9a3b26370a8d92
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-12-08 19:30:38 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -46,13 +46,12 @@ let handler = async (m, { conn, usedPrefix }) => {
     for (let tag in grupos) grupos[tag].sort((a, b) => a.localeCompare(b))
 
     const secciones = Object.entries(grupos).map(([tag, cmds]) => {
-  const emoji = emojis[tag] || '⭐'
-  return `╭─⛄━━━━━━━━━━━━🎄
-│ 🎄 〔 ${emoji} ${tag.toUpperCase()} 〕 ❄️
-│━━━━━━━━━━━━━━━━❄️\n`
-   + cmds.map(cmd => `│ 🎁 ${cmd}`).join('\n') 
-   + `\n╰─🎅━━━━━━━━━━━━☃️`
-}).join('\n\n')
+      const emoji = emojis[tag] || '🌱'
+      return `
+> ${emoji} ${tag.toUpperCase()}
+${cmds.map(cmd =>  ۫'🌴ֹ 𔐼ֹ֪➩ \`\`\${cmd}`).join('\n')}\`\`\`
+`
+    }).join('\n')
 
 let menuText = `
 ╔══❄️☃️════••════☃️❄️══╗
