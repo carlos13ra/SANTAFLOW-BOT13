a2770b4a47c6d32be9f467c5b42deb464560e417
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-12-08 19:22:07 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -48,7 +48,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     const secciones = Object.entries(grupos).map(([tag, cmds]) => {
       const emoji = emojis[tag] || '❄️'
       return `
-> ${emoji} ${tag.toUpperCase()}
+${emoji} ${tag.toUpperCase()}
 ${cmds.map(cmd => `✎ ${cmd}`).join('\n')}
 `
     }).join('\n')
